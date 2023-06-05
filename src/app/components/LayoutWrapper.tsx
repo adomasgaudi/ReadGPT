f9a3215b033c0fc72daf6c03e10b322597034e4e
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2023-06-05 12:34:58 +0300

    feta: added sidebar

--- src/app/components/LayoutWrapper.tsx
+++ src/app/components/LayoutWrapper.tsx
@@ -3,23 +3,11 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 
 const App = ({ children }: any) => {
-  const [theme, setTheme] = useState(true)
+  // const [theme, setTheme] = useState(true)
 
   return (
-    <body className={`bg-main text-main ${theme ? 'light' : 'dark'}`}>
-      <div className="container mx-auto max-w-[1000px] h-full">
-        <header className='flex justify-between container p-2 pl-5 pr-9 border-b border-gray-200'>
-          <Link href="/">
-            <div className=''>
-              BookGPT
-            </div>
-          </Link>
-          <button className='' onClick={() => setTheme(prev => !prev)}>
-            theme
-          </button>
-        </header>
-        {children}
-      </div>
+    <body className={`bg-main text-main light`}>
+      {children}
     </body>
   )
 }
--- src/app/knyga2/components/BookGPT.tsx
+++ src/app/knyga2/components/BookGPT.tsx
@@ -4,8 +4,12 @@ import tw, { css } from "twin.macro";
 import { fontNotoSerifJp } from "@/app/css/twinStyles";
 import { Box, Slider, SliderTrack, SliderFilledTrack, SliderThumb, Flex } from "@chakra-ui/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faCaretUp, faCaretDown, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import Link from "next/link";
 
+const SidebarContainer = tw.div`absolute top-0 left-0 h-screen w-[300px] z-10 border-r `
+const MenuButton = tw.button`fixed top-0 left-0 py-2 px-4 z-20`
+const CloseButton = tw.button`py-2 px-4 mt-2`
 
 function divideBySentence(text) {
   // Split text by "。", "！", "？", and map over the results to trim whitespace and add the punctuation back
@@ -22,32 +26,60 @@ function divideBySentence(text) {
   return formattedSentences;
 }
 
-
 export default function BookGPT({ text }: any) {
+  const [theme, setTheme] = useState(true)
   const containerRef = useRef(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
 
   // State for slider value
   const [sliderValue, setSliderValue] = useState(0);
 
-  return (
-    <Flex>
-      <div>
-        <div className="flex justify-center">
-          <button className=' rounded-full h-[50px] w-[50px]'>
-            <FontAwesomeIcon icon={faCaretUp} />
-          </button>
-        </div>
-        <div className="flex">
+  // State for selected number
+  const [selectedNumber, setSelectedNumber] = useState(1);
 
-          <div ref={containerRef} className="container pt-1 pl-5 text-xl" css={[fontNotoSerifJp]}>{divideBySentence(text).join(" ")}</div>
-          <Box width="30px">
-            <Slider
-              aria-label="slider-ex-4"
-              orientation="vertical"
-              defaultValue={30}
-              onChange={(val) => setSliderValue(val)}
-              height="300px"
-              css={css`
+  // List of numbers
+  const numbers = Array.from({ length: 16 }, (_, i) => i + 1);
+
+  return (
+    <div className="container mx-auto max-w-[1000px] h-full">
+      <header className='flex justify-between container p-2 pl-5 pr-9 border-b border-gray-200'>
+        <button className='' onClick={() => setIsSidebarOpen(true)}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <Link href="/">
+          <div className=''>
+            ReadGPT
+          </div>
+        </Link>
+        <button className='' onClick={() => setTheme(prev => !prev)}>
+          theme
+        </button>
+      </header>
+      {isSidebarOpen && (
+        <SidebarContainer css={['background: white;']}>
+          <CloseButton onClick={() => setIsSidebarOpen(false)} className="absolute top-0 right-[-43px] border-2 rounded flex justify-center items-center h-10 w-10 ml-2" css={['background: white']}>
+            <FontAwesomeIcon icon={faXmark} />
+          </CloseButton>
+        </SidebarContainer>
+      )}
+
+      <Flex className="w-full pb-[100px]">
+        <div className="w-full">
+          <div className="flex justify-center">
+            <button className=' rounded-full h-[50px] w-[50px]'>
+              <FontAwesomeIcon icon={faCaretUp} />
+            </button>
+          </div>
+          <div className="flex">
+            <div ref={containerRef} className="container pt-1 pl-5 text-xl" css={[fontNotoSerifJp]}>{divideBySentence(text).join(" ")}</div>
+            <Box width="30px">
+              <Slider
+                aria-label="slider-ex-4"
+                orientation="vertical"
+                defaultValue={30}
+                onChange={(val) => setSliderValue(val)}
+                height="300px"
+                css={css`
             opacity: 0.1;
             transition: opacity 0.3s ease-in-out;
 
@@ -57,24 +89,38 @@ export default function BookGPT({ text }: any) {
               opacity: 1;
             }
           `}
-            >
-              <SliderTrack bg="gray.100">
-                <SliderFilledTrack bg="gray.200" />
-              </SliderTrack>
-              <SliderThumb boxSize={6}>
-                <Box color="gray.300" as="span">
-                  {sliderValue}
-                </Box>
-              </SliderThumb>
-            </Slider>
-          </Box>
+              >
+                <SliderTrack bg="gray.100">
+                  <SliderFilledTrack bg="gray.200" />
+                </SliderTrack>
+                <SliderThumb boxSize={6}>
+                  <Box color="gray.300" as="span">
+                    {sliderValue}
+                  </Box>
+                </SliderThumb>
+              </Slider>
+            </Box>
+          </div>
+          <div className="flex justify-center">
+            <button className=' rounded-full h-[50px] w-[50px]'>
+              <FontAwesomeIcon icon={faCaretDown} />
+            </button>
+          </div>
+
+          <div className="flex flex-row flex-nowrap max-w-full w-full overflow-scroll p-5 " css={[tw`border-2`]}>
+            {numbers.map(number => (
+              <div
+                key={number}
+                className={`flex min-w-[40px] min-h-[40px] items-center justify-center border border-gray-200 ${selectedNumber === number ? 'bg-gray-300' : ''}`}
+                onClick={() => setSelectedNumber(number)}
+              >
+                {number}
+              </div>
+            ))}
+          </div>
+
         </div>
-        <div className="flex justify-center">
-          <button className=' rounded-full h-[50px] w-[50px]'>
-            <FontAwesomeIcon icon={faCaretDown} />
-          </button>
-        </div>
-      </div>
-    </Flex>
+      </Flex>
+    </div>
   );
 }
