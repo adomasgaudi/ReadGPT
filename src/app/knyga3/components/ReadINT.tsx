1380217cbad5745a9b59a42af7542b8fb0f65ea0
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2023-06-06 16:36:06 +0300

    feta: buttons, scrolling

--- src/app/knyga3/components/ReadINT.tsx
+++ src/app/knyga3/components/ReadINT.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faCaretDown, faCaretUp, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 import Link from 'next/link' // @ts-ignore
 import tw, { css } from 'twin.macro'
 import ReadINTInfra from './ReadINTInfra'
 import { 悪魔の弟子 } from './text'
+import { fontNotoSerifJp } from '@/app/css/twinStyles'
+
+const textContent = 悪魔の弟子
 
 const ss = {
   e1stl: css`border-bottom: 1px solid #e2e8f0;`,
@@ -15,6 +18,8 @@ const ss = {
   e2loc: css`${tw`absolute top-0 right-[-43px] ml-2 mt-2`}`,
   e3in: css`${tw`flex justify-between items-start p-2 pl-5 pr-9`}`,
   e3stl: css` border-bottom: 1px solid #e2e8f0;`,
+  e4stl: css`${tw`rounded-full`}`,
+  e4form: css`${tw`h-[50px] w-[50px]`}`,
 }
 
 const ButtonSidebarClose = ({ setIsSidebarOpen }: any) =>
@@ -25,6 +30,24 @@ const ButtonSidebarClose = ({ setIsSidebarOpen }: any) =>
     <FontAwesomeIcon icon={faXmark} />
   </button>
 
+const ButtonDown = ({ pagePartPos, allPages, selectedPagePos, setPagePartPos }: any) =>
+  <button
+    css={[ss.e4stl, ss.e4form]}
+    onClick={() => setPagePartPos(prev => prev + 1)}
+    disabled={pagePartPos + 1 === allPages[selectedPagePos].length}
+  >
+    <FontAwesomeIcon icon={faCaretDown} />
+  </button>
+const ButtonUp = ({ pagePartPos, setPagePartPos }: any) =>
+  <button
+    css={[ss.e4stl, ss.e4form]}
+    onClick={() => setPagePartPos((prev: any) => prev - 1)}
+    disabled={pagePartPos === 0}
+  >
+    <FontAwesomeIcon icon={faCaretUp} />
+  </button>
+
+const Center = tw.div`flex justify-center items-center`
 //
 
 //
@@ -37,10 +60,28 @@ const ButtonSidebarClose = ({ setIsSidebarOpen }: any) =>
 
 export default function ReadINT() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [pagePos, setPagePos] = useState(0)
+  const [pagePartPos, setPagePartPos] = useState(0)
+  const allPages = textContent.reduce((acc: any, chapter: any) => acc.concat(chapter.pages), [])
+
+  const [selectedPagePos, setSelectedPagePos] = useState(1)
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const scrollHeight = containerRef.current.scrollHeight
+      const elementHeight = containerRef.current.offsetHeight
+      const numberOfElements = allPages[pagePos].length
+
+      const scrollPos = (scrollHeight - elementHeight) / (numberOfElements - 1) * pagePartPos
+
+      containerRef.current.scrollTo({ top: scrollPos, behavior: 'smooth' })
+    }
+  }, [pagePartPos])
   return (
 
     <ReadINTInfra
-      text={悪魔の弟子}
+      text={textContent}
       {...{ isSidebarOpen }}
       childHeader={
         <>
@@ -57,12 +98,25 @@ export default function ReadINT() {
       childSidebar={
         <>
           <ButtonSidebarClose {...{ setIsSidebarOpen }} />
-          <div>hi sidebar
+          <div>
+            hi sidebar
           </div>
         </>
       }
     >
-      <div>hi main</div>
+      <div>
+        <Center>
+          <ButtonUp {...{ setPagePartPos, pagePartPos }} />
+        </Center>
+        <div ref={containerRef} css={[tw`h-80 overflow-scroll`]}>
+          {allPages[pagePos].map((page, index) => (
+            <p key={index} css={[tw`bg-blue`, tw`p-2`, fontNotoSerifJp]}>{page}</p>
+          ))}
+        </div>
+        <Center>
+          <ButtonDown {...{ setPagePartPos, pagePartPos, allPages, selectedPagePos }} />
+        </Center>
+      </div>
     </ ReadINTInfra>
   )
 }
