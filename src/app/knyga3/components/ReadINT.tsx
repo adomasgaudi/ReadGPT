5ddd4422f3826502685d723ab3cf341e0435e526
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2023-06-16 14:33:53 +0300

    feta: added pages

--- src/app/knyga3/components/ReadINT.tsx
+++ src/app/knyga3/components/ReadINT.tsx
@@ -12,6 +12,7 @@ const textContent = 悪魔の弟子
 const ins = {
   center: css`${tw`flex justify-center items-center`}`,
   e3: css`${tw`flex justify-between items-start p-2 pl-5 pr-9`}`,
+  // e4: css`${tw`flex justify-center items-center `}`,
 }
 
 const ss = {
@@ -19,6 +20,7 @@ const ss = {
   e2: css`background: white; ${tw`border-2 rounded`}`,
   e3: css` border-bottom: 1px solid #e2e8f0;`,
   e4: css`${tw`rounded-full`}`,
+  e5: css`background: var(--bg-main); ${tw`border rounded-full border-[#000]`}`,
 }
 
 const ButtonSidebarClose = ({ setIsSidebarOpen }: any) =>
@@ -34,7 +36,12 @@ const ButtonSidebarClose = ({ setIsSidebarOpen }: any) =>
   </button>
 const gradientStyles = (deg: any) => css`
   background: red;
-  background: linear-gradient(${deg}deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%);`
+  background: linear-gradient(
+    ${deg}deg, var(--bg-main-opq) 20%, 
+    var(--bg-main-semi-opq) 65%, 
+    var(--bg-main-trn) 100%
+  );
+  `
 
 const ButtonUpBlock = ({ setPagePartPos, pagePartPos, setSelectedPagePos }: any) =>
   <div css={[tw`absolute top-0 w-full -mb-10 z-10`, ins.center, gradientStyles(180)]} >
@@ -51,11 +58,14 @@ const ButtonUpBlock = ({ setPagePartPos, pagePartPos, setSelectedPagePos }: any)
   </div>
 
 const ButtonDownBlock = ({
-  setPagePartPos, pagePartPos, allPages, selectedPagePos, setSelectedPagePos,
+  setPagePartPos, pagePartPos, allPages, selectedPagePos, setSelectedPagePos, setIsPagesVisible,
 }: any) =>
   <div css={[tw`absolute bottom-10 w-full -mb-10 z-10`, ins.center, gradientStyles(0)]} >
+    <button css={[tw`w-[50px]`]}>
+
+    </button>
     <button
-      css={[ss.e4, tw`h-[50px] w-[50px]`]}
+      css={[ss.e4, ss.e5, tw` w-[50px] m-2`]}
       onClick={() => {
         setPagePartPos((prev: any) => prev + 1)
         setSelectedPagePos((prev: any) => prev + 1)
@@ -63,6 +73,9 @@ const ButtonDownBlock = ({
       disabled={pagePartPos + 1 === allPages[selectedPagePos].length}
     >
       <FontAwesomeIcon icon={faCaretDown} />
+    </button>
+    <button css={[tw`w-[50px]`, ss.e5]} onClick={() => setIsPagesVisible((prev: any) => !prev)}>
+      <FontAwesomeIcon icon={faBars} />
     </button>
   </div>
 
@@ -80,6 +93,7 @@ export default function ReadINT() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
   const [pagePos, setPagePos] = useState(0)
   const [pagePartPos, setPagePartPos] = useState(0)
+  const [isPagesVisible, setIsPagesVisible] = useState(false)
   const allPages = textContent.reduce((acc: any, chapter: any) => acc.concat(chapter.pages), [])
 
   const [selectedPagePos, setSelectedPagePos] = useState(0)
@@ -110,7 +124,7 @@ export default function ReadINT() {
 
   const buttonDownLogic = () => pagePartPos + 1 !== allPages[selectedPagePos].length
     && <ButtonDownBlock {...{
-      setPagePartPos, setSelectedPagePos, pagePartPos, allPages, selectedPagePos,
+      setPagePartPos, setSelectedPagePos, pagePartPos, allPages, selectedPagePos, setIsPagesVisible,
     }}
     />
   const buttonUpLogic = () => pagePartPos !== 0
@@ -163,6 +177,22 @@ export default function ReadINT() {
         {buttonDownLogic()}
 
       </div>
+      {isPagesVisible && (
+        <div className="flex flex-row flex-nowrap max-w-full w-full overflow-scroll p-5 " css={[tw`border-2`]}>
+          {allPages.map((item: any, idx: any) => (
+            <button
+              key={idx}
+              className={`flex min-w-[40px] m-1 min-h-[40px] items-center justify-center border border-gray-200 ${selectedPagePos + 1 === item ? 'bg-gray-300' : ''}`}
+              onClick={() => {
+                setSelectedPagePos(idx + 1)
+                setPagePartPos(0)
+              }}
+            >
+              {idx + 1}
+            </button>
+          ))}
+
+        </div>)}
     </ ReadINTInfra >
   )
 }
