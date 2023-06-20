7f22e50b1437b215b8f49312cda3a412c008f38c
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2023-06-20 18:03:12 +0300

    fix: rename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const App = () => {
       <div className='pt-[100px]'>
         {[
           { href: '/knyga', title: 'functionality' },
-          { href: '/knyga3', title: 'new guy' },
+          { href: '/devils_desciple', title: '悪魔の弟子' },
         ].map((item, index) =>
           <Link key="index" href={item.href}>
             <div className="m-2 border p-2"> {item.title}</div>
